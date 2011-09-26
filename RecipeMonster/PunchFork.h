d240e492174090df6d88486e4b91830a8d8114a5
Mark Aufflick <mark@aufflick.com>
2011-09-26 14:54:49 +1000

    adding template plist

---
+++ RecipeMonster/PunchFork.h
@@ -0,0 +1,13 @@
+//
+//  PunchFork.h
+//  RecipeMonster
+//
+//  Created by Mark Aufflick on 26/09/11.
+//  Copyright 2011 Pumptheory Pty Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PunchFork : NSObject
+
+@end
---
+++ RecipeMonster/PunchFork.m
@@ -0,0 +1,15 @@
+//
+//  PunchFork.m
+//  RecipeMonster
+//
+//  Created by Mark Aufflick on 26/09/11.
+//  Copyright 2011 Pumptheory Pty Ltd. All rights reserved.
+//
+
+#import "PunchFork.h"
+
+@implementation PunchFork
+
+
+
+@end
